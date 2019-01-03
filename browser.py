9092a36cdc5ad119299e386a51ccb79ca9c296ec
timebird7 <timebird7@gmail.com>
2019-01-03 09:53:20 +0900

    first commit

---
+++ browser.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+url = "https://search.naver.com/search.naver?query="
+momo = ['혜빈', '연우', '제인', '태하', '나윤', '데이지', '아인', '주이', '낸시']
+for x in momo:
+    webbrowser.open(url + x)
+
+
+
+#webbrowser.open(url)
